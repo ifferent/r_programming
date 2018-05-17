018ba07acfe98bc226a70861c75fb57691422e19
roddy.hung <roddy.hung@protech.com.tw>
2018-05-17 11:02:06 +0800

    Add the ch3 & ch4 sample code into github
      Issue title(No.): no issue
    
      Type: add
    
      Note:
          Ch3 is about the basic graphics in R.
          Ch4 is using the ggplot2 package to create new types of graphic
          specifically.
    
      Changes to be committed:
          new file:   ch3/ch3.R
          new file:   ch4/ch4.R

---
+++ ch3/ch3.R
@@ -0,0 +1,110 @@
+library(readr)
+library(dplyr)
+library(plotrix)
+
+############################################檔案載入與設定################################################
+
+mysample.exp1_path="presentation_sample_ppt/sample_data/family/最近一年內曾因家庭緣故影響工作之情形－按無法加班或無法延長工時分(年齡).csv"
+mysample.exp1_1_path="presentation_sample_ppt/sample_data/family/最近一年內曾因家庭緣故影響工作之情形－按中斷工作或上班時臨時趕回家分(年齡).csv"
+mysample.exp2_path="presentation_sample_ppt/sample_data/myself/ch3_exp1.csv"
+mysample.exp3_path="presentation_sample_ppt/sample_data/myself/ch3_exp2.csv"
+
+mysample.exp1<-read_csv(mysample.exp1_path,col_names = FALSE)
+mysample.exp1_nocolname<-read_csv(mysample.exp1_1_path,col_names=TRUE)
+mysample.exp1_1<-read_csv(mysample.exp1_1_path,col_names=TRUE)
+mysample.exp2<-read_csv(mysample.exp2_path,col_names=TRUE)
+mysample.exp3<-read_csv(mysample.exp3_path,col_names=TRUE)
+
+##########################################################################################################
+
+age_range<-c("20－24歲","25－29歲","30－34歲","35－39歲","40－44歲","45－49歲","50－54歲","55－59歲","60－64歲","65歲及以上")
+freq_tag<-c("經常","有時","極少","從不")
+barplot(mysample.exp1_nocolname$"經常")
+barplot(mysample.exp1_nocolname$"經常",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故影響無法加班或延長工時之情形(經常)", names.arg=age_range)
+barplot(mysample.exp1_nocolname$"有時",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故影響無法加班或延長工時之情形(有時)", names.arg=age_range)
+barplot(mysample.exp1_nocolname$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故影響無法加班或延長工時之情形(極少)", names.arg=age_range)
+barplot(mysample.exp1_nocolname$"從不",xlab="年齡區間",ylab="計數",horiz=TRUE,main="主計處─因家庭緣故影響無法加班或延長工時之情形(從不)", names.arg=age_range)#放橫的
+
+barplot(mysample.exp1_nocolname$"經常",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故影響無法加班或延長工時之情形(經常)", names.arg=age_range, legend=mysample.exp1_nocolname$"項目", col=c("gray","green","blue","red","cyan","yellow","pink","magenta","black","aliceblue"))#legend & color 的使用
+
+bind_table<-cbind(mysample.exp1_nocolname$"經常",mysample.exp1_nocolname$"有時",mysample.exp1_nocolname$"極少",mysample.exp1_nocolname$"從不")
+
+barplot(t(bind_table),xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故影響無法加班或延長工時之情形", names.arg=age_range, legend=freq_tag, col=c("gray","green","red","blue"),beside=TRUE) #複式長條圖
+
+##########################################################################################################
+par(mfrow=c(2,2))
+barplot(mysample.exp1_1$"經常",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(經常)", names.arg=age_range)
+barplot(mysample.exp1_1$"有時",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(有時)", names.arg=age_range)
+barplot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)", names.arg=age_range)
+barplot(mysample.exp1_1$"從不",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(從不)", names.arg=age_range)
+
+par(mfrow=c(4,1))
+barplot(mysample.exp1_1$"經常",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(經常)", names.arg=age_range)
+barplot(mysample.exp1_1$"有時",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(有時)", names.arg=age_range)
+barplot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)", names.arg=age_range)
+barplot(mysample.exp1_1$"從不",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(從不)", names.arg=age_range)
+
+par(mfrow=c(1,4))
+barplot(mysample.exp1_1$"經常",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(經常)", names.arg=age_range)
+barplot(mysample.exp1_1$"有時",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(有時)", names.arg=age_range)
+barplot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)", names.arg=age_range)
+barplot(mysample.exp1_1$"從不",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(從不)", names.arg=age_range)
+
+par(mfrow=c(3,2))
+barplot(mysample.exp1_1$"經常",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(經常)", names.arg=age_range)
+barplot(mysample.exp1_1$"有時",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(有時)", names.arg=age_range)
+barplot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)", names.arg=age_range)
+barplot(mysample.exp1_1$"從不",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(從不)", names.arg=age_range)
+
+
+hist(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",breaks=50)
+
+##########################################################################################################
+par(mfrow=c(1,1))
+mysample.exp1.percent<-prop.table(mysample.exp1_nocolname$"極少")*100
+age_percent<-paste(age_range," ",round(mysample.exp1.percent,2),"%",sep="")
+pie(mysample.exp1.percent,xlab="年齡區間",labels=age_percent,main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",init.angle=0)
+
+fan.plot(round(mysample.exp1.percent),labels=age_percent,main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",ticks=500)
+
+##########################################################################################################
+
+boxplot(mysample.exp3$銷售額)
+
+#########################################################################################################
+
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額")#散佈圖
+par(mfrow=c(3,3))
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",pch=3)
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",pch=6)
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",pch=9)
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",pch=11)
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",pch=12)
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",pch=18)
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",pch=19)
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",pch='*')
+plot(mysample.exp3$銷售額,xlab="銷售額",ylab="計數",main="公司銷售額",pch='%')
+
+
+#########################################################################################################
+
+plot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",type='l',col="red")
+plot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",type='h',col="blue")
+plot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",type='b',col="yellow")
+plot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",type='s',col="green")
+plot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",type='l',col="pink",lwd=3)
+plot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",type='l',col="black",lwd=5)
+plot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",type='l',col="gray30",lwd=2,lty=2)
+plot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",type='l',col="gray50",lwd=5,lty=3,xlim=c(3,5))
+plot(mysample.exp1_1$"極少",xlab="年齡區間",ylab="計數",main="主計處─因家庭緣故中斷工作或上班時臨時趕回家(極少)",type='l',col="gray80",lwd=7,lty=4,xlim=c(3,5),ylim=c(550,650))
+
+
+
+
+
+
+
+
+
+
+
---
+++ ch4/ch4.R
@@ -0,0 +1,76 @@
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(lubridate)
+
+############################################檔案載入與設定################################################
+
+ch4sample.exp1_path="presentation_sample_ppt/sample_data/family/最近一年內曾因家庭緣故影響工作之情形－按無法加班或無法延長工時分(年齡).csv"
+ch4sample.exp1_1_path="presentation_sample_ppt/sample_data/family/最近一年內曾因家庭緣故影響工作之情形－按中斷工作或上班時臨時趕回家分(年齡).csv"
+#ch4sample.exp1_2_path="presentation_sample_ppt/sample_data/family/最近一年內曾因家庭緣故影響工作之情形－按無法加班或無法延長工時分(ggplot2).csv"
+ch4sample.exp2_path="presentation_sample_ppt/sample_data/myself/ch3_exp1.csv"
+ch4sample.exp3_path="presentation_sample_ppt/sample_data/myself/ch3_exp2.csv"
+ch4sample.exp4_path="presentation_sample_ppt/sample_data/myself/ch3_exp3.csv"
+ch4sample.exp5_path="presentation_sample_ppt/sample_data/消費者物價基本分類暨項目群指數/消費者物價基本分類暨項目群指數.csv"
+
+ch4sample.exp1<-read_csv(ch4sample.exp1_path,col_names = TRUE)
+ch4sample.exp1_1<-read_csv(ch4sample.exp1_1_path,col_names=TRUE)
+#ch4sample.exp1_2<-read_csv(ch4sample.exp1_2_path,col_names=TRUE)
+ch4sample.exp2<-read_csv(ch4sample.exp2_path,col_names=TRUE)
+ch4sample.exp3<-read_csv(ch4sample.exp3_path,col_names=TRUE)
+ch4sample.exp4<-read_csv(ch4sample.exp4_path,col_names=TRUE)
+ch4sample.exp5<-read_csv(ch4sample.exp5_path,col_names=TRUE)
+
+##########################################################################################################
+age_range<-c("20－24歲","25－29歲","30－34歲","35－39歲","40－44歲","45－49歲","50－54歲","55－59歲","60－64歲","65歲及以上")
+freq_tag<-c("經常","有時","極少","從不")
+
+ggplot(ch4sample.exp1,aes(x=項目別,y=經常))+geom_bar(stat="identity")
+ggplot(ch4sample.exp1,aes(x=項目別,y=有時,fill=項目別))+geom_bar(stat="identity")
+ch4sample.exp1_gatable<-gather(ch4sample.exp1,freq_tag,key="頻率",value="計數") #將表格轉成聚集形式
+ch4sample.exp1_gatable<-rename(ch4sample.exp1_gatable, "年齡區間"="項目別")
+
+ggplot(ch4sample.exp1_gatable,aes(x=年齡區間,y=計數,fill=頻率))+geom_bar(position="dodge",stat="identity")
+ch4sample.exp1_gatable$頻率<-factor(ch4sample.exp1_gatable$頻率,levels=freq_tag)#改變頻率順序
+ggplot(ch4sample.exp1_gatable,aes(x=年齡區間,y=計數,fill=頻率))+geom_bar(position="dodge",stat="identity")
+
+ggplot(ch4sample.exp1_gatable,aes(x=年齡區間,y=計數,fill=頻率))+geom_bar(stat="identity")#沒有position="dodge"
+
+ggplot(ch4sample.exp1_gatable,aes(x=年齡區間,y=計數,fill=頻率))+geom_bar(position="dodge", colour="black"
+      ,stat="identity")#colour="black"=>長條圖加黑色邊框
+
+ggplot(ch4sample.exp1_gatable,aes(x=年齡區間,y=計數,fill=頻率))+geom_bar(position="dodge", colour="black"
+      ,stat="identity") + scale_fill_brewer(palette="Accent")#scale_fill_brewer(palette="Accent")=>長條圖圖色(調色盤)
+
+ggplot(ch4sample.exp1_gatable,aes(x=年齡區間,y=計數,fill=頻率)) + geom_bar(position="dodge", colour="black"
+       ,stat="identity") + scale_fill_brewer(palette="Accent") + xlab("Age Range")
+
+ggplot(ch4sample.exp1_gatable,aes(x=年齡區間,y=計數,fill=頻率)) + geom_bar(position="dodge", colour="black"
+       ,stat="identity", size=1, width=1)#size=1, width=1=>外框粗細，與長條圖區間寬度
+
+ggplot(ch4sample.exp1_gatable,aes(x=年齡區間,y=計數,fill=頻率))
+
+######################### Line Graphs #########################################
+item.gather<-c("年月","總指數","米類及其製品","肉類","蔬菜")
+item.seq<-c("總指數","米類及其製品","肉類","蔬菜")
+
+ch4sample.exp5<-mutate(ch4sample.exp5, "年月" = make_date(年,月))
+
+ggplot(ch4sample.exp5,aes(x=年月,y=總指數))+geom_line()
+
+ch4sample.exp5_gatable<-select(ch4sample.exp5, item.gather)
+ch4sample.exp5_gatable<-gather(ch4sample.exp5_gatable,item.seq,key="品項",value="年增率") #將表格轉成聚集形式
+ch4sample.exp5_gatable$品項<-factor(ch4sample.exp5_gatable$品項,levels=item.seq)#改變頻率順序
+
+ggplot(ch4sample.exp5_gatable,aes(x=年月,y=年增率,colour=品項))+geom_line()+scale_colour_brewer(palette="Set2")
+
+ggplot(ch4sample.exp5_gatable,aes(x=年月,y=年增率,group=品項))+geom_line(colour="blue",linetype="dashed")
+
+###################################################################################
+
+ggplot(ch4sample.exp5,aes(x=年,y=總指數))+geom_point()
+
+#ggplot(ch4sample.exp5_gatable,aes(x=年,y=年增率,colour=品項))+geom_point()
+
+

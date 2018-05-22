847e2afb0d0db83c4d6712c38712b1c97da2ef97
roddy.hung <roddy.hung@protech.com.tw>
2018-05-22 19:37:13 +0800

    Change the file path
      Issue title(No.): no issue
    
      Type: modify
    
      Note:
          Because we need to satisfy the R project default file path, we
    	  should to modify it.
    
      Changes to be committed:
          modified:   ch2/ch2.R

--- ch2/ch2.R
+++ ch2/ch2.R
@@ -1,25 +1,15 @@
-show_repeat<-rep(3,5)
-show_repeat
-a_set<-c(0.5,-3,25,78,10)
-a_seq<-c(1:5)
-a_set.string<-c("Hellow","everyone","!")
-null_set<-NA
-a_mixture<-list(a_set,a_seq,a_set.string,null_set)
-
-mean(sample(1:sample(1:exp(5),1), sample(1:log(1500,exp(1)),1)))
-
-#############################################################################
-############## 
-
 library("readr")
-library("tidyverse")
 
-path.Ch2_exp_table1.csv<-"D:/my_code/R_code/presentation_sample_ppt/ch2/Ch2_exp_table1.csv"
+############################################檔案載入與設定################################################
 
-mych2.csv<-read_csv(path.Ch2_exp_table1.csv)
+ch2sample.exp1_path<-"ch2/sample_data0/Ch2_exp_table1.csv"
+
+##########################################################################################################
+
+mych2.csv<-read_csv(ch2sample.exp1_path)
 #mych2.csv<-read_csv("D:/my_code/R_code/presentation_sample_ppt/ch2/Ch2_exp_table1.csv")
 
-mych2.csv2<-read.csv(path.Ch2_exp_table1.csv,header=TRUE)
+mych2.csv2<-read.csv(ch2sample.exp1_path,header=TRUE)
 #mych2.csv2<-read.csv("D:/my_code/R_code/presentation_sample_ppt/ch2/Ch2_exp_table1.csv",header=TRUE)
 
 mych2.csv3<-read.csv(path.Ch2_exp_table1.csv,header=FALSE)

d7363b5915d3a82b515ccfdf012fc8ce369b3242
roddy.hung <roddy.hung@protech.com.tw>
2018-05-25 17:10:06 +0800

    Add more sample function
      Issue title(No.): no issue
    
      Type: modify
    
      Note:
          Add function select(), summarize(), group_by()
    
      Changes to be committed:
          modified:   ch2/ch2.R

--- ch2/ch2.R
+++ ch2/ch2.R
@@ -1,4 +1,5 @@
 library("readr")
+library("dplyr")
 
 ############################################檔案載入與設定################################################
 
@@ -12,12 +13,29 @@ mych2.csv<-read_csv(ch2sample.exp1_path)
 mych2.csv2<-read.csv(ch2sample.exp1_path,header=TRUE)
 #mych2.csv2<-read.csv("D:/my_code/R_code/presentation_sample_ppt/ch2/Ch2_exp_table1.csv",header=TRUE)
 
-mych2.csv3<-read.csv(path.Ch2_exp_table1.csv,header=FALSE)
+mych2.csv3<-read.csv(ch2sample.exp1_path,header=FALSE)
 #mych2.csv3<-read.csv("D:/my_code/R_code/presentation_sample_ppt/ch2/Ch2_exp_table1.csv",header=FALSE)
 
 filter(mych2.csv, month==12, day==25)
 
-filter(mych2.csv, month==2, dep_delay<=0)  
-  
-  
+filter(mych2.csv, month==2, dep_delay<=0)
+
+our.view<-filter(mych2.csv2, month==12, day==25)#這是使用R預設的函數讀取的表格
+head(our.view)
+
+select(mych2.csv,month,day,arr_time)
+
+select(mych2.csv,year:dep_time,air_time:time_hour)
+
+summarize(mych2.csv, delay=mean(dep_delay, na.rm=TRUE))  
+
+by_day<-group_by(mych2.csv, year ,month, day)
+summarize(by_day, delay=mean(dep_delay, na.rm=TRUE))  
+
+
+
+
+
+
+
 

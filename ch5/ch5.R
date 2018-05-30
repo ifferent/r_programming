249136d81b41a87bf1cadc8699baafaad09834bc
roddy.hung <roddy.hung@protech.com.tw>
2018-05-30 18:03:15 +0800

    CH5 code example temp1
      Note:
          Try too add the function:sample and rnorm code example.
    
      On branch draft/ch5
      Changes to be committed:
          modified:   ch5/ch5.R

--- ch5/ch5.R
+++ ch5/ch5.R
@@ -1,6 +1,15 @@
 library(readr)
-
+library(tibble)
+library(ggplot2)
 ################################檔案載入與設定#################################
 
 
 ###############################################################################
+score_range=1:100
+
+sample(score_range,50,replace=FALSE)
+student.score <- sample(score_range,50,replace=TRUE)
+#sample(score_range,50),預設replace是FALSE
+
+mean(student.score)
+ggplot

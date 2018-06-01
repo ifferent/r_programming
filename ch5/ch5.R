1421425b1e0bba35d72da3584c1b7693fa4aadcc
roddy.hung <roddy.hung@protech.com.tw>
2018-06-01 16:12:37 +0800

    Write the ch5 code exapmle   Issue title(No.): no issue
      Type: draft(1)
    
      Note:
    
      On branch draft/ch5
      Changes to be committed:
          modified:   ch5/ch5.R

--- ch5/ch5.R
+++ ch5/ch5.R
@@ -1,15 +1,31 @@
 library(readr)
+library(dplyr)
 library(tibble)
 library(ggplot2)
 ################################檔案載入與設定#################################
 
 
 ###############################################################################
+
 score_range=1:100
 
-sample(score_range,50,replace=FALSE)
+sample(score_range,100,replace=FALSE)
 student.score <- sample(score_range,50,replace=TRUE)
 #sample(score_range,50),預設replace是FALSE
 
-mean(student.score)
-ggplot
+transcript <- tibble(score=student.score)
+mean(transcript$score)
+ggplot(transcript,aes(score))+geom_histogram(stat="bin",bins=10, fill="white",colour="black")+geom_line(color="red",stat="bin",bins=10)
+
+###############################################################################
+
+student.score<-round(rnorm(50,mean=80,sd=10),0)
+transcript$score<-ifelse(student.score>100,100,student.score)
+mean(transcript$score)
+ggplot(transcript,aes(score))+geom_histogram(stat="bin",bins=10, fill="white",colour="black")+geom_line(color="red",stat="bin",bins=10)
+
+
+
+
+
+

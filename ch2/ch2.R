35974e4bb88dcaf20f0a435d83a23b23daef79a7
Roddy_Hung <ifferent_4@hotmail.com>
2018-06-14 11:40:08 +0800

    Add the function cbind and summary
      Issue title(No.): no issue
    
      Type: add
    
      Note:
          1. The function use the base package.
          2. Compare with this two function to select and summarize.
    
      On branch draft/ch2
      Changes to be committed:
          modified:   ch2/ch2.R

--- ch2/ch2.R
+++ ch2/ch2.R
@@ -15,6 +15,14 @@
 
 source("common/check_package.R")#檢查是否有未安裝的套件
 
+###############################################################################
+####                                                                       ####
+#### 載入套件相關使用函數參考:                                             ####
+#### readr: read_csv                                                       ####
+#### dplyr: filter, seleclt, summarize, group_by                           ####
+####                                                                       ####
+###############################################################################
+
 library("readr")#載入readr套件
 library("dplyr")#載入dplyr套件
 
@@ -26,7 +34,7 @@ mych2.csv<-read_csv(ch2sample.exp1_path)
 #檔案完整路徑如下：
 #mych2.csv<-read_csv("D:/my_code/R_code/presentation_sample_ppt/ch2/Ch2_exp_table1.csv")
 
-mych2.csv2<-read.csv(ch2sample.exp1_path,header=TRUE)#這是使用R預設base套件的函數讀取的表格
+mych2.csv2<-read.csv(ch2sample.exp1_path,header=TRUE)#這是使用R預設utils套件的函數讀取的表格
 #檔案完整路徑如下：
 #mych2.csv2<-read.csv("D:/my_code/R_code/presentation_sample_ppt/ch2/Ch2_exp_table1.csv",header=TRUE)
 
@@ -44,8 +52,12 @@ our.view<-filter(mych2.csv2, month==12, day==25)
 head(our.view)
 
 select(mych2.csv,month,day,arr_time)
+#這是使用R預設base套件的cbind函數讀取的表格
+temp_view <- cbind(mych2.csv$month, mych2.csv$day, mych2.csv$arr_time)
+head(temp_view)
 
 select(mych2.csv,year:dep_time,air_time:time_hour)
+#cbind沒辦法這樣使用cbind(mych2.csv$year:mych2.csv$dep_time, mych2.csv$air_time:mych2.csv$time_hour)
 
 summarize(mych2.csv, delay=mean(dep_delay, na.rm=TRUE))  
 #引數na.rm有沒有要包含缺失值 
@@ -58,6 +70,8 @@ sum.by_day_mon<-summarize(by_day_mon, delay=mean(dep_delay, na.rm=TRUE))
 
 by_day_mon_year<-group_by(mych2.csv, year ,month, day)
 sum.by_day_mon_year<-summarize(by_day_mon_year, delay=mean(dep_delay, na.rm=TRUE))  
+
+summary(mych2.csv)#比較一下R預設base套件的summary函數的摘要表格
 
 
 

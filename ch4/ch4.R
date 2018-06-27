6bb92ba25f891e44c84b9bcc8a922aa20db7f8ae
Roddy_Hung <ifferent_4@hotmail.com>
2018-06-27 18:00:29 +0800

    Add the histogram, line grpah and scatter plot example code.
      Issue title(No.): no issue
    
      Type: add
    
      Note:
    
      On branch draft/ch4
      Changes to be committed:
          modified:   ch4/ch4.R

--- ch4/ch4.R
+++ ch4/ch4.R
@@ -1,9 +1,9 @@
 ###############################################################################
 ###############################################################################
 ####                                                                       ####
-#### 完成日期: 2018-06-25                                                  ####
+#### 完成日期: 2018-06-27                                                  ####
 #### 作者：Roddy Hung                                                      ####
-#### 版本：V3.0                                                            ####
+#### 版本：V3.3                                                            ####
 ####                                                                       ####
 #### 第4章範例程式:                                                        ####
 ####    1.基礎繪圖(圖形)文法的概念                                         ####
@@ -16,7 +16,10 @@
 ####        i.堆疊長條圖(引數position的使用)                               ####
 ####       ii.顏色設定                                                     ####
 ####      iii.外框大小和長條圖寬度的設定                                   ####
-####    4.線圖                                                             ####
+####    4.直方圖                                                           ####
+####        i.統計轉換的概念                                               ####
+####    5.線圖                                                             ####
+####    6.散佈圖                                                           ####
 ####                                                                       ####
 ###############################################################################
 ###############################################################################
@@ -43,19 +46,19 @@ library(lubridate)
 
 ch4sample.exp1_path="ch4/sample_data/最近一年內曾因家庭緣故影響工作之情形－按無法加班或無法延長工時分(年齡).csv"
 ch4sample.exp1_1_path="ch4/sample_data/最近一年內曾因家庭緣故影響工作之情形－按中斷工作或上班時臨時趕回家分(年齡).csv"
-ch4sample.exp2_path="ch4/sample_data/消費者物價基本分類暨項目群指數.csv"
-ch4sample.exp3_path="ch4/sample_data/台灣太陽光電發電量統計表.csv"
+ch4sample.exp2_path="ch4/sample_data/成績單.csv"
+ch4sample.exp3_path="ch4/sample_data/消費者物價基本分類暨項目群指數.csv"
+ch4sample.exp4_path="ch4/sample_data/台灣太陽光電發電量統計表.csv"
 
 ch4sample.exp1<-read_csv(ch4sample.exp1_path,col_names = TRUE)
 ch4sample.exp1_1<-read_csv(ch4sample.exp1_1_path,col_names=TRUE)
 ch4sample.exp2<-read_csv(ch4sample.exp2_path,col_names=TRUE)
 ch4sample.exp3<-read_csv(ch4sample.exp3_path,col_names=TRUE)
+ch4sample.exp4<-read_csv(ch4sample.exp4_path,col_names=TRUE)
 
 ##########################################################################################################
 
 ggplot(ch4sample.exp1,aes(x=項目別,y=經常)) + geom_bar(stat="identity")
-ggplot(ch4sample.exp1,aes(x=項目別,y=經常)) + stat_identity(geom="bar")
-
 
 ggplot(ch4sample.exp1,aes(x=項目別,y=有時,fill=項目別)) + geom_bar(stat="identity")
 ggplot(ch4sample.exp1,aes(x=項目別,y=有時,fill=有時)) + geom_bar(stat="identity")
@@ -103,30 +106,75 @@ ggplot(ch4sample.exp1_gatable,aes(x=年齡區間,y=人數,colour=頻率)) +
 
 ggplot(ch4sample.exp1,aes(x=項目別,y=有時,colour=有時)) +
     geom_bar(stat="identity",fill="white",width=2)
+########################### Histogram #########################################
+subject_tag<-c("國文","數學","歷史","地理")
+ch4sample.exp2_gatable<-gather(ch4sample.exp2,subject_tag,key="科目",value="分數") #將表格轉成聚集形式
+
+ggplot(ch4sample.exp2,aes(x=國文)) +
+  geom_histogram(fill="white", colour="black")
+ggplot(ch4sample.exp2,aes(x=國文)) +
+  geom_histogram(fill="white", colour="black", binwidth=10)
+
+ggplot(ch4sample.exp2_gatable,aes(x=分數,fill=科目)) +
+  geom_histogram(position="identity", binwidth=15, alpha=0.3)
+
+
+ggplot(ch4sample.exp2,aes(x=國文)) +
+  geom_bar(stat="bin", bins=5, fill="white", colour="black")
+
+ggplot(ch4sample.exp2,aes(x=國文)) + 
+    stat_bin(geom="bar", bins=5, fill="white", colour="black")
+
+ggplot(ch4sample.exp2_gatable,aes(x=分數,fill=科目)) +
+    geom_bar(stat="bin", bins=8, position="identity", alpha=0.3)
+
+ggplot(ch4sample.exp2_gatable,aes(x=分數,fill=科目)) + 
+    stat_bin(geom="bar", bins=8, position="identity", alpha=0.3)
 
 ######################### Line Graphs #########################################
 item.gather<-c("年月","總指數","米類及其製品","肉類","蔬菜")
 item.seq<-c("總指數","米類及其製品","肉類","蔬菜")
 
-ch4sample.exp2<-mutate(ch4sample.exp2, "年月" = make_date(年,月))
+ch4sample.exp3<-mutate(ch4sample.exp3, "年月" = make_date(年,月))
 
-ggplot(ch4sample.exp2,aes(x=年月,y=總指數))+geom_line()
+ggplot(ch4sample.exp3,aes(x=年月,y=總指數))+geom_line()
 
-ch4sample.exp2_gatable<-select(ch4sample.exp2, item.gather)
-ch4sample.exp2_gatable<-gather(ch4sample.exp2_gatable,item.seq,key="品項",value="年增率") #將表格轉成聚集形式
+ch4sample.exp3_gatable<-select(ch4sample.exp3, item.gather)
+ch4sample.exp3_gatable<-gather(ch4sample.exp3_gatable,item.seq,key="品項",value="年增率") #將表格轉成聚集形式
 
-ggplot(ch4sample.exp2_gatable,aes(x=年月,y=年增率,colour=品項)) +
+ggplot(ch4sample.exp3_gatable,aes(x=年月,y=年增率,colour=品項)) +
     geom_line() +
         guides(colour=guide_legend(reverse = TRUE)) #改變legend(品項)順序
-#ch4sample.exp2_gatable$品項<-factor(ch4sample.exp2_gatable$品項,levels=item.seq)#改變legend(品項)順序另一個方法
+#ch4sample.exp3_gatable$品項<-factor(ch4sample.exp3_gatable$品項,levels=item.seq)#改變legend(品項)順序另一個方法
 
-ggplot(ch4sample.exp2_gatable,aes(x=年月,y=年增率,colour=品項,linetype=品項)) +
+ggplot(ch4sample.exp3_gatable,aes(x=年月,y=年增率,colour=品項,linetype=品項)) +
     geom_line() +
         guides(colour=guide_legend(reverse = TRUE), linetype=guide_legend(reverse = TRUE))
 
-ggplot(ch4sample.exp2_gatable,aes(x=年月,y=年增率,group=品項)) +
+ggplot(ch4sample.exp3_gatable,aes(x=年月,y=年增率,group=品項)) +
     geom_line(colour="blue",linetype="dashed")
 
 ###################################################################################
 
-ggplot(ch4sample.exp2,aes(x=年,y=總指數))+geom_point()
+ch4sample.exp4<-mutate(ch4sample.exp4, "年月" = make_date(年度,月份))
+
+ch4sample.exp4_ponhu<-filter(ch4sample.exp4, 光電站名稱== "澎湖光電")
+ggplot(ch4sample.exp4_ponhu,aes(x=年月, y=平均單位裝置容量每日發電量)) +
+    geom_point()
+
+ggplot(ch4sample.exp4,aes(x=年月, y=平均單位裝置容量每日發電量, colour=光電站名稱)) +
+    geom_point()
+
+ch4sample.exp4$年月<-factor(ch4sample.exp4$年月)
+ggplot(ch4sample.exp4,aes(x=年月, y=平均單位裝置容量每日發電量, colour=光電站名稱,group=1)) +
+  geom_point()
+
+ggplot(ch4sample.exp4,aes(x=年月, y=平均單位裝置容量每日發電量, colour=光電站名稱, shape=光電站名稱)) +
+  geom_point()
+
+
+
+
+
+
+
